abbb18208cfcaa17a033759212aded696e5e8aed
tanveerhusyn <tanveerhussain465@gmail.com>
2024-03-24 01:52:13 +0500

    first commit

---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,48 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/icon-01.svg",
+    title: "Centralized Review Dashboard",
+    description:
+      "Effortlessly gather and manage reviews from various platforms in one centralized dashboard, providing a holistic view of your customer feedback.",
+  },
+  {
+    id: 2,
+    icon: "/images/icon/icon-02.svg",
+    title: "AI-Driven Response Crafting",
+    description:
+      "Leverage artificial intelligence to generate thoughtful, personalized responses to reviews, saving time while enhancing customer relations.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/icon-03.svg",
+    title: "Multi-Platform Integration",
+    description:
+      "Seamlessly fetch and respond to reviews across all major review sites and social platforms, ensuring your brand's presence is uniformly excellent everywhere.",
+  },
+  {
+    id: 4,
+    icon: "/images/icon/icon-04.svg",
+    title: "Advanced Analytics & Insights",
+    description:
+      "Gain deep insights into customer sentiment and review trends with our advanced analytics tools, helping you make informed decisions.",
+  },
+  {
+    id: 5,
+    icon: "/images/icon/icon-05.svg",
+    title: "Efficient Workflow Automation",
+    description:
+      "Streamline your review management process with automation features that help prioritize responses and alert you to critical feedback.",
+  },
+  {
+    id: 6,
+    icon: "/images/icon/icon-06.svg",
+    title: "Ongoing Feature Enhancements",
+    description:
+      "Benefit from continuous improvements and new features, ensuring your review management system remains cutting-edge without additional cost.",
+  },
+];
+
+export default featuresData;
