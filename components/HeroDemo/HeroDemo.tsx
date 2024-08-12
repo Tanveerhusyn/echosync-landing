3f8f32710f371478f587cb4d95f3cdf63818ff70
tanveerhusyn <tanveerhussain465@gmail.com>
2024-08-12 17:09:38 +0500

    fixes

---
+++ components/HeroDemo/HeroDemo.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { ContainerScroll } from "../ui/container-scroll-animation";
+import Image from "next/image";
+
+export default function HeroScrollDemo() {
+  return (
+    <div className="flex flex-col overflow-hidden">
+      <ContainerScroll>
+        <Image
+          src={`/preview.png`}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto h-full rounded-2xl object-cover object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
+  );
+}
